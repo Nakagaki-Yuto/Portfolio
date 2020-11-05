953bc75c25686aee0dcd220a9833c23a0896fb8a
Nakagaki-Yuto <yuto.nakagaki@gmail.com>
2020-11-06 01:46:39 +0900

    edit settings

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -133,6 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+
 try:
     from .local_settings import *
 except ImportError:
